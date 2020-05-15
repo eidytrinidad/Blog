248f68289b743cbbc7efedce3ed095710fe22bfe
eidytrinidad <eidyjtf@gmail.com>
2020-05-14 21:52:41 -0400

    Moving IMG File and Component Folder to source, adding logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 import './css/App.css'
+import Header from './components/Header';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      
-    </div>
+      <Route path="/" component={Header}/>
+
+      </div>
+    </Router>
   );
 }
 
